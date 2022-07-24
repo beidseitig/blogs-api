2a9d9ca813e738ccb2a5e5c18ca230f7158d075c
Ramos <beidseitig@gmail.com>
2022-07-24 18:07:04 -0300

    feat: req02 - user.js model

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,6 +1,7 @@
 'use strict';
 const User = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
+    id: DataTypes.STRING,
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
